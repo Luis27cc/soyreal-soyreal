40c1cd8864f42846bcf86861e67ecd97fe5c7187
Luis27cc <luiscampodel@gmail.com>
2025-02-17 00:35:23 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import TestAutoestima from "./pages/TestAutoestima";
+import Galeria from "./pages/Galeria";
+import Foro from "./pages/Foro";
+import Recursos from "./pages/Recursos";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test-autoestima" element={<TestAutoestima />} />
+        <Route path="/galeria" element={<Galeria />} />
+        <Route path="/foro" element={<Foro />} />
+        <Route path="/recursos" element={<Recursos />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
